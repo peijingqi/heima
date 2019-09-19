65a8138288955d2cfafb3f9c6f57b5db5d12f14a
裴景琪 <879273795@qq.com>
2019-09-19 17:43:18 +0800

    调整目录结构

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router' // 路由
 import store from './store'
 
 Vue.config.productionTip = false
